89c8d348e6006f710415bdd8cd10cff02552e596
Toshi_K <130676600+BZ-kozai-T@users.noreply.github.com>
2023-12-21 11:57:07 +0100

    Update cp_tracking_kymo.py

--- cp_tracking_kymo.py
+++ cp_tracking_kymo.py
@@ -35,6 +35,7 @@ def butter_lowpass_filter(signal, cutoff, length, order = 4):
     return filtered_signal
 
 def track_cp_kymo(inputdir, data_name, pixel_size, pixels, bottom_pixels, border, distance_from_center, cutoff_length):
+  
     kymo = skio.imread(inputdir + "/" + data_name + ".tiff", plugin="tifffile")
     nrows, nlines = kymo.shape
 
@@ -49,22 +50,25 @@ def track_cp_kymo(inputdir, data_name, pixel_size, pixels, bottom_pixels, border
         lowpass_filtered_list[:, iline] = filtered_signal
 
     y = []
-
     for iline in range(nlines):
 
         local_maxima, _ = find_peaks(lowpass_filtered_list[pixels:(nrows - bottom_pixels), iline], height = border)
         print("detected local maxima positions:", local_maxima)
+      
         if len(local_maxima) > 1:
             max_list = []
+          
             for i in range(len(local_maxima)):
                 v = lowpass_filtered_list[local_maxima[i] + pixels, iline]
                 max_list.append(v)
             max_v = max(max_list)
             local_maxima = np.where(lowpass_filtered_list[:, iline] == max_v)[0][0]
             print("local maximum position:", local_maxima)
+          
         elif len(local_maxima) == 1:
             local_maxima = local_maxima[0] + pixels
             print("local maximum position:", local_maxima)
+          
         else:
             local_maxima = None # no local maxima detected
             print("local maximum position:", local_maxima)
@@ -72,22 +76,29 @@ def track_cp_kymo(inputdir, data_name, pixel_size, pixels, bottom_pixels, border
 
     distance_center = np.arange(-distance_from_center, distance_from_center + 0.1, pixel_size)
     distance_list = []
+  
     for i in range(len(y)):
+      
         if y[i] == None:
             continue
+          
         distance = distance_center[y[i]]
         distance_list.append(distance)
 
     # flip the location for the radial function
     radius = np.arange(0, distance_from_center + 0.1, pixel_size)
     flipped_position_list = []
+  
     for j in range(len(y)):
+      
         if y[j] == None:
             continue
+          
         if y[j] < nrows//2:
             flip = (nrows//2) - y[j]
             distance_radius = radius[flip]
             flipped_position_list.append(distance_radius)
+          
         else:
             distance_radius = radius[y[j] - nrows//2]
             flipped_position_list.append(distance_radius)
